e0a7dc652dfbc38d07126dcaa05d1ad76e0f313f
Hamad Fuad <hamad.ali@procco.com>
2022-04-19 10:48:59 +0300

    Initial Commit

---
+++ Shared/iOSTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTemplateApp.swift
+//  Shared
+//
+//  Created by Hamad Ali on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
